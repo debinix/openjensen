728f5fa06fea06151c9eec940448f7dcdeb22b19
debinix <bertil.kronlund@gmail.com>
2014-02-04 19:29:45 +0100

    Set encoding as cli parameter

--- php/assets/database.php
+++ php/assets/database.php
@@ -1,7 +1,7 @@
 <?php
 // mysql_connect("localhost", "root", "") or die(mysql_error());
 // mysql_select_db("scrum") or die(mysql_error());
-pg_connect("host=localhost dbname=openjensen user=jensen") or die('Could not connect: ' . pg_last_error());
+pg_connect("host=localhost dbname=openjensen user=jensen options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 
 // mysql_query("SET NAMES utf8");
 pg_query("SET NAMES UTF8");
