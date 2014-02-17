1ea9f28dceb67a4867f9868dba3cda16dad8aafe
debinix <bertil.kronlund@gmail.com>
2014-02-17 08:18:54 +0100

    Add curl to single student POST

--- php/course.php
+++ php/course.php
@@ -32,6 +32,33 @@ if($_SESSION['usertype_id'] == 1)
       // wait until file is written at server before continue to read it.
       //
       
+      $user_id = $_SESSION['user_id'];
+      $user_program = $_SESSION['user_program'];
+      $url = 'http://www.mc-butter.se/cgi-bin/cgi-list-betygelev.cgi';
+      $fields = array( 'user_id' => urlencode($user_id),
+                       'user_program' => urlencode($user_program)
+                      );
+      //url-ify the data for the POST
+      foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+      rtrim($fields_string, '&');
+      //open connection
+      $ch = curl_init();
+      
+      //set the url, number of POST vars, POST data
+      curl_setopt($ch,CURLOPT_URL, $url);
+      curl_setopt($ch,CURLOPT_POST, count($fields));
+      curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+      
+      //execute post
+      $result = curl_exec($ch);
+      if($result === false)
+          $Error->set("Curl have a problem to reach $url") ;
+      
+      //close connection
+      curl_close($ch);
+      // sleep to make sure we can continue with our php code below
+      sleep(2);
+      
       $course_row = file($betyg_elev_file);
            
       // loop through the array
