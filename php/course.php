3f9a1422e667802d489a0c396a10ea279eacbcbc
debinix <bertil.kronlund@gmail.com>
2014-02-17 11:04:45 +0100

    Build POST query with php build-in

--- php/course.php
+++ php/course.php
@@ -39,17 +39,20 @@ if($_SESSION['usertype_id'] == 1)
       $fields = array( 'user_id' => urlencode($user_id),
                        'user_program' => urlencode($user_program)
                       );
-      //url-ify the data for the POST
-      foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-      rtrim($fields_string, '&');
+      
+      //url-ify the data for the POST 8with php built-in function
+      $fields_string = http_build_query($fields);
+      
+      // foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+      // rtrim($fields_string, '&');
       //open connection
       $ch = curl_init();
       
       //set the url, number of POST vars, POST data
       curl_setopt($ch,CURLOPT_URL, $url);
       curl_setopt($ch,CURLOPT_POST, count($fields));
-      curl_setopt($ch,CURLOPT_POSTFIELDS, "user_id=3&user_program=2");
-      // curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+      // curl_setopt($ch,CURLOPT_POSTFIELDS, "user_id=3&user_program=2");
+      curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
       
       //execute post
       $result = curl_exec($ch);
