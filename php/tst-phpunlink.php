54d87124a9536f35b3d9a9e4145960d58b853a80
debinix <bertil.kronlund@gmail.com>
2014-02-22 11:11:48 +0100

    Fix logic in test code

--- php/tst-phpunlink.php
+++ php/tst-phpunlink.php
@@ -15,17 +15,24 @@
     $removefilename = "/home/jensen/www.mc-butter.se/public_html/phpunlinktest.OK" ;    
 
     if(!file_exists($removefilename)) {
-        echo "Before unlink test: Missing $removefilename <br>" ; 
+        echo "Missing testfile $removefilename (Run: cgi-bin/cgi-test-php-unlink.cgi first)<br>" ; 
     }
     else {
-        echo "Before unlink test: Found file $removefilename <br>" ;         
+        echo "BEFORE unlink test: Found test file $removefilename <br><br>" ;         
+        
+        // Delete the file
         unlink($removefilename);
-
-        echo "";
-        echo "After unlink test: File $removefilename is deleted! <br>";
+        
+        if(file_exists($removefilename)) {
+            echo "SUCCESS: File $removefilename is DELETED! <br>";
+        }
+        else {
+            echo "ERROR: Could not remove $removefilename <br>";
+        }
+        
     }
     
-    echo "";
+    echo "<br>";
     echo "Test finished! <br>"
 
 
